dd9abb14dfadad1b444590e75513545c36223207
Porter Thomas <pthoma24@byu.edu>
2023-02-16 18:18:39 -0700

    Add project files.

---
+++ Block.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace PCoin
+{
+    class Block
+    {
+        // Create index list reference for each block in the chain (i.e. genesis block index is 0)
+        public int Index { get; set; }
+
+        // Create the hash that connects the current block to the previous block
+        public string PreviousHash { get; set; }
+
+        // Create a timestamp for each block
+        public string Timestamp { get; set; }
+
+        // Create each block's hash
+        public string Hash { get; set; }
+
+        // Create the Nonce
+        public int Nonce { get; set; }
+
+        public List<Transaction> Transactions { get; set; }
+
+        public Block(int index, string timestamp, List<Transaction> transactions, string previousHash = "")
+        {
+            this.Index = index;
+            this.Timestamp = timestamp;
+            this.Transactions = transactions;
+            this.PreviousHash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+        }
+
+        // Actually calculate the hash
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.PreviousHash + this.Timestamp + this.Transactions.ToString() + this.Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0, difficulty) != new string('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                // Console.WriteLine("Mining: " + this.Hash);
+            }
+
+            Console.WriteLine("Block has been mined: " + this.Hash);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+namespace PCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 = new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+            
+            Blockchain pcoin = new Blockchain(3, 100); // First parameter is the difficulty. 2 means that hashes will have to be generated until there are 2 zeros at the beginning of the hash
+
+            Console.WriteLine("Start the Miner.");
+            pcoin.MinePendingTransactions(wallet1);
+            Console.WriteLine("\nBalance of wallet1 is $" + pcoin.GetBalanceOfWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            pcoin.addPendingTransaction(tx1);
+            Console.WriteLine("Start the Miner.");
+            pcoin.MinePendingTransactions(wallet2);
+            Console.WriteLine("\nBalance of wallet1 is $" + pcoin.GetBalanceOfWallet(wallet1).ToString());
+            Console.WriteLine("\nBalance of wallet2 is $" + pcoin.GetBalanceOfWallet(wallet2).ToString());
+
+            string blockJSON = JsonConvert.SerializeObject(pcoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            // pcoin.GetLatestBlock().PreviousHash = "12345";
+
+            // Run validation function
+            if (pcoin.IsChainValid())
+            {
+                Console.WriteLine("Blockchain is Valid!");
+            }
+            else
+            {
+                Console.WriteLine("Blockchain is NOT valid.");
+            }
+        }
+    }
+}
